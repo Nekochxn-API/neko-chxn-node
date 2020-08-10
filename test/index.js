5f924220550d46d351e942bf9270b1356644e092
Venny <sgtmattis@gmail.com>
2020-08-10 20:27:47 +0200

    Add test

---
+++ test/index.js
@@ -0,0 +1,18 @@
+const neko = require('..');
+
+let success = true;
+const test = async (description, fn) => {
+	try {
+		await fn();
+		console.log(`SUCCESS: ${description}`);
+	} catch (error) {
+		console.error(`FAIL: ${description}`);
+		success = false;
+	}
+};
+
+Promise.all(
+	Object.keys(neko)
+		.filter(key => typeof neko[key] === 'function')
+		.map(key => test(`Test ${key}`, neko[key]))
+).finally(() => process.exit(success ? 0 : 1));
--- tests/neko-chxn.test.js
+++
@@ -1,3 +0,0 @@
-const neko = require('../dist/neko-chxn.js');
-
-void neko.dance().then(console.log);
